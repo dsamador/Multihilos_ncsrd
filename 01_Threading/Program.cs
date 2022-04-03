873ae519f7b081cf8bdcea5fa9dab5dcd841076b
David Amador <54488581+dsamador@users.noreply.github.com>
2022-04-03 18:37:34 -0500

    Hilos 2

---
+++ 01_Threading/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading; // 
+
+namespace _01_Threading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {           
+            //Forma de pasar datos al hilo
+            Thread hilo2 = new Thread(MiMensaje);
+            //al iniciar el hilo mandamos el mensaje
+            hilo2.Start(5);
+        }
+        // Se ejecuta en el segundo hilo y recibe parametro
+        static void MiMensaje(object o)
+        {
+            int m = Convert.ToInt32(o);
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine($"Saludos desde el mensaje {m}, {m * 2}");
+        }
+        static void Saludos()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Saludos desde el hilo");
+        }        
+    }
+}
---
+++ 02_Threading_Multiples_Hilos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace _02_Threading_Multiples_Hilos
+{
+    internal class Program
+    {
+        private static bool ejecutar = true;
+        static void Main(string[] args)
+        {
+            int m = 0;
+            int x = 0;
+
+            // Creamos el hilo y lo ararncamos
+            Thread miHilo = new Thread(Mensaje);
+            miHilo.Start();
+
+            //simula que main lleva a cabo sus propias acciones
+            while (ejecutar)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"Saludos desde hilo principal {m}");
+                m++;
+                if(m == 500)
+                    ejecutar = false;
+            }
+        }
+        static void Mensaje()
+        {
+            int n = 0;
+            while (ejecutar)
+            {                
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Saludos desde hilo {n}");
+                n++;
+            }
+        }
+    }
+}
